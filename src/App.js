97f415115ae7d17acf7e7547a0cf224254199e12
Suchandan91 <mondalsuchandan91@gmail.com>
2023-03-21 14:29:20 +0530

    first commit
    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+        <h1>"WELCOME TO eDgeWrapper"</h1>
+        <p>CODE SUCCESSFULLY DEPLOYED!!!</p>
+    
+    
+    </>
+  );
+}
+
+export default App;
